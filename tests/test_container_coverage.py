b626e699c96c4b16bcbc26641a0c9dbdb4282973
T151232 <dperezcabrera@gmail.com>
2025-11-29 14:10:36 +0100

    added unit testing

---
+++ tests/test_container_coverage.py
@@ -0,0 +1,57 @@
+import pytest
+import contextvars
+from pico_ioc import PicoContainer, ComponentFactory, ScopedCaches, ScopeManager, health
+from pico_ioc.exceptions import AsyncResolutionError
+from pico_ioc.locator import ComponentLocator
+
+def test_container_ashutdown():
+    c = PicoContainer(ComponentFactory(), ScopedCaches(), ScopeManager())
+    
+    async def run():
+        await c.ashutdown()
+        assert c.container_id not in PicoContainer.all_containers()
+        
+    import asyncio
+    asyncio.run(run())
+
+def test_export_graph_no_locator():
+    c = PicoContainer(ComponentFactory(), ScopedCaches(), ScopeManager())
+    with pytest.raises(RuntimeError, match="No locator attached"):
+        c.export_graph("out.dot")
+
+def test_resolve_args_edge_cases():
+    c = PicoContainer(ComponentFactory(), ScopedCaches(), ScopeManager())
+    assert c._resolve_args(()) == {}
+    
+    locator = ComponentLocator({}, {})
+    c.attach_locator(locator)
+    assert c._resolve_args(()) == {}
+
+def test_info_logging(caplog):
+    c = PicoContainer(ComponentFactory(), ScopedCaches(), ScopeManager())
+    with caplog.at_level("INFO"):
+        c.info("test message")
+    assert "test message" in caplog.text
+
+def test_scope_context_manager():
+    c = PicoContainer(ComponentFactory(), ScopedCaches(), ScopeManager())
+    
+    with c.scope("request", "req-1"):
+        assert c.scopes.get_id("request") == "req-1"
+    
+    assert c.scopes.get_id("request") is None
+
+def test_health_check_exception():
+    c = PicoContainer(ComponentFactory(), ScopedCaches(), ScopeManager())
+    
+    class Unhealthy:
+        # FIX: Use decorator
+        @health
+        def check(self):
+            raise ValueError("fail")
+            
+    obj = Unhealthy()
+    c._caches._singleton.put("key", obj)
+    
+    res = c.health_check()
+    assert res["key.check"] is False
---
+++ tests/test_scope_coverage.py
@@ -0,0 +1,66 @@
+import pytest
+import contextvars
+from pico_ioc.scope import ScopeManager, ScopedCaches, ComponentContainer
+from pico_ioc.exceptions import ScopeError
+from pico_ioc import cleanup
+
+def test_scope_manager_validation():
+    manager = ScopeManager()
+    
+    with pytest.raises(ScopeError, match="non-empty"):
+        manager.register_scope("")
+        
+    with pytest.raises(ScopeError, match="reserved"):
+        manager.register_scope("singleton")
+
+    manager.register_scope("request")
+    manager.register_scope("request")
+
+def test_scope_manager_reserved_interactions():
+    manager = ScopeManager()
+    
+    assert manager.get_id("singleton") is None
+    assert manager.activate("singleton", "id") is None
+    assert manager.deactivate("singleton", None) is None
+
+def test_scope_manager_unknown_scope():
+    manager = ScopeManager()
+    
+    with pytest.raises(ScopeError, match="Unknown scope"):
+        manager.deactivate("unknown_scope", None)
+        
+    with pytest.raises(ScopeError, match="Unknown scope"):
+        manager.activate("unknown_scope", "id")
+
+def test_scoped_caches_shrink():
+    caches = ScopedCaches()
+    manager = ScopeManager()
+    
+    caches.shrink("singleton", 10)
+    
+    scope_name = "request"
+    manager.activate(scope_name, "req1")
+    
+    c1 = caches.for_scope(manager, scope_name)
+    
+    caches._by_scope[scope_name] = {
+        "req1": ComponentContainer(),
+        "req2": ComponentContainer()
+    }
+    
+    caches.shrink(scope_name, 1)
+    assert len(caches._by_scope[scope_name]) == 1
+
+def test_cleanup_container_exceptions():
+    caches = ScopedCaches()
+    container = ComponentContainer()
+    
+    class BrokenCleanup:
+        @cleanup
+        def cleanup(self):
+            raise ValueError("Boom")
+            
+    obj = BrokenCleanup()
+    container.put("key", obj)
+    
+    caches._cleanup_container(container)
